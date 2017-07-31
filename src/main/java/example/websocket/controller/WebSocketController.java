f00cd4fbbc9734c7ee698fb05a04f848a41a0221
sukaye <skcdfc@gmail.com>
2017-07-31 16:12:56 +0800

    use SimpMessagingTemplate

--- src/main/java/example/websocket/controller/WebSocketController.java
+++ src/main/java/example/websocket/controller/WebSocketController.java
@@ -5,16 +5,29 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebSocketController {
 	@Autowired
-    private SimpMessagingTemplate messagingTemplate;
+	private SimpMessagingTemplate messagingTemplate;
 
 	@MessageMapping("/broadcast")
 	@SendTo("/topic/getMessage")
 	public String broadcast(String message) {
 		return message;
+	}
+
+	/**
+	 * another way to send message use SimpMessagingTemplate to broadcast
+	 * message
+	 * 
+	 * @param message
+	 */
+	@RequestMapping("/broadcast2")
+	@ResponseBody
+	public void test(String message) {
+		messagingTemplate.convertAndSend("/topic/getMessage", message);
 	}
 }
